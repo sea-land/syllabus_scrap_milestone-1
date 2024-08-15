bd63c0675f4f7dae821e5efb0405bb724ef34de3
Takaki Kaito <nbtmktrt33312@gmail.com>
2024-08-15 23:49:41 +0900

    変更点 ・フリガナ列、授業タイプの追加 ・process_scheduleの統一 ・出力ファイルを学部ごとに変更 ・見出し行の追加

--- work/scrap.py
+++ work/scrap.py
@@ -3,6 +3,7 @@ import csv
 import datetime
 import time
 import subprocess
+from fugashi import Tagger
 from logging import getLogger, handlers, Formatter, DEBUG, ERROR
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -47,25 +48,21 @@ def get_current():
     return now.year, now.month
 
 
-def init_csv_file(year, term):
-    with open(f"../data/syllabus_data_{year}_{term}.csv", "a", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        writer.writerow(["学部", "コースコード", "科目名", "担当教員", "学期", "曜日時限", "授業概要"])
-
-
-def scrape_syllabus_data(driver, dest):
+def scrape_syllabus_data(driver, dest_dir):
     log("Accessing Waseda's syllabus\n")
     driver.get("https://www.wsl.waseda.jp/syllabus/JAA101.php")
-    with open(dest, "w", newline="", encoding="utf-8") as file:
-        writer = csv.writer(file)
-        for name in faculties:
-            log(f"Accessing {name} syllabus")
+    for faculty in faculties:
+        log(f"Accessing {faculty} syllabus")
+        faculty_file = f"{dest_dir}/raw_syllabus_data_{faculty}.csv"
+        with open(faculty_file, "w", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow(["学部", "コースコード", "科目名", "担当教員", "学期", "曜日時限", "授業概要"])
             select = Select(driver.find_element(By.NAME, "p_gakubu"))
-            select.select_by_visible_text(name)
+            select.select_by_visible_text(faculty)
             driver.execute_script("func_search('JAA103SubCon');")
             driver.execute_script("func_showchg('JAA103SubCon', '1000');")
 
-            log(f"Scraping {name} data.")
+            log(f"Scraping {faculty} data.")
             start_time = time.time()
             total_elements = 0
             while True:
@@ -76,7 +73,7 @@ def scrape_syllabus_data(driver, dest):
                     for row in rows[1:]:
                         cols = row.find_all("td")
                         writer.writerow([
-                            name,
+                            faculty,
                             cols[1].text.strip(),
                             cols[2].text.strip(),
                             cols[3].text.strip(),
@@ -87,7 +84,7 @@ def scrape_syllabus_data(driver, dest):
                     driver.find_element(By.XPATH, "//*[@id='cHonbun']/div[2]/table/tbody/tr/td[3]/div/div/p/a").click()
                 except NoSuchElementException:
                     break
-            log(f"Total Number of Subjects {name}: {total_elements}")
+            log(f"Total Number of Subjects {faculty}: {total_elements}")
             log(f"Finished in {time.time() - start_time:.6f} seconds\n")
             driver.find_element(By.CLASS_NAME, "ch-back").click()
 
@@ -100,36 +97,36 @@ def convert_zen_to_han(source_path, output_path):
             writer.writerow([zen_to_han(cell, kana=False) for cell in row])
 
 
-def process_schedule1(row):
-    schedule = str(row[4])
-    common_data = row[:4]
+def process_schedule(row):
+    schedule = str(row[5])
+    common_data = row[:5]
     expanded_rows = []
 
     if ":" in schedule:
+        # ':' が含まれている場合の処理
         for time in schedule.split(":"):
             if len(time) < 4:
                 continue
             time = time.replace("　", " ").replace("\n", " ")
             new_schedule = time.split(" ")[0]
             expanded_rows.append(common_data + [new_schedule])
-    else:
-        expanded_rows.append(list(row))
-    return expanded_rows
-
-
-def process_schedule2(row):
-    schedule = str(row[4])
-    common_data = row[:4]
-    expanded_rows = []
-    if "-" in schedule:
+    elif "-" in schedule:
+        # '-' が含まれている場合の処理
         schedule = schedule.replace(" ", "")
         day, time_range = schedule[0], schedule[1:]
-        start, end = map(int, time_range.split("-"))
-        for time in range(start, end + 1):
-            new_schedule = f"{day}{time}時限"
-            expanded_rows.append(common_data + [new_schedule])
+        try:
+            start, end = map(int, time_range.split("-"))
+            for time in range(start, end + 1):
+                new_schedule = f"{day}{time}時限"
+                expanded_rows.append(common_data + [new_schedule])
+        except ValueError:
+            # 変換エラーをキャッチし、元のデータを追加
+            log(f"スケジュール範囲 '{time_range}' の処理中にエラーが発生しました。行: {row}", ERROR)
+            expanded_rows.append(list(row))
     else:
+        # ':' や '-' が含まれていない場合、そのまま追加
         expanded_rows.append(list(row))
+
     return expanded_rows
 
 
@@ -148,19 +145,49 @@ def check_versions():
 
 
 def format_syllabus_data(source_path, dest_path):
+    tagger = Tagger()
+
+    class_type_map = {
+        "L": "講義",
+        "S": "演習/ゼミ",
+        "W": "実習/実験/実技",
+        "F": "外国語",
+        "P": "実践/フィールドワーク",
+        "G": "研究",
+        "T": "論文",
+        "B": "対面+オンデマンド",
+        "O": "オンデマンド",
+        "X": "その他"
+    }
+
+    def get_furigana(text):
+        words = tagger(text)
+        furigana = "".join([word.feature.kana if word.feature.kana else word.surface for word in words])
+        return furigana
+
+    def get_class_type(code):
+        class_type_code = code[-1]
+        return class_type_map.get(class_type_code, "不明")
+
     with open(source_path, "r", newline="", encoding="utf-8") as source, open(dest_path, "w", newline="", encoding="utf-8") as dest:
         csvreader = csv.reader(source)
         csvwriter = csv.writer(dest)
-        log("Formatting data.")
         rows = list(csvreader)
-        for row in rows:
+        # ヘッダーの書き込み
+        csvwriter.writerow(rows[0][:3] + ["かもくめい"] + rows[0][3:6] + ["授業形態"] + rows[0][6:])
+        
+        for row in rows[1:]:
             try:
                 han_row = [zen_to_han(cell, kana=False) for cell in row]
-                fmt1 = process_schedule1(han_row)
-                for sub_row in fmt1:
-                    fmt2 = process_schedule2(sub_row)
-                    for final_row in fmt2:
-                        csvwriter.writerow(final_row)
+                subject_name = han_row[2]
+                furigana = get_furigana(subject_name)
+                course_code = han_row[1]
+                class_type = get_class_type(course_code)
+                han_row.insert(3, furigana)
+                han_row.insert(6, class_type)
+                fmt = process_schedule(han_row)
+                for sub_row in fmt:
+                    csvwriter.writerow(sub_row)
             except Exception as e:
                 log(f"Error processing row: {row} - {e}", ERROR)
 
@@ -171,9 +198,7 @@ def main():
     try:
         check_versions()
         year, month = get_current()
-        dest_dir = "../data"
-        raw_dir = f"{dest_dir}/raw_syllabus_data_{year}_{month}.csv"
-        out_dir = f"{dest_dir}/syllabus_data_{year}_{month}.csv"
+        dest_dir = f"../data/{year}_{month}"
 
         os.makedirs(dest_dir, exist_ok=True)
 
@@ -189,11 +214,16 @@ def main():
         log(f"Error : {e}", ERROR)
 
     try:
-        scrape_syllabus_data(driver, raw_dir)
+        scrape_syllabus_data(driver, dest_dir)
     finally:
         driver.quit()
 
-    format_syllabus_data(raw_dir, out_dir)
+    for faculty in faculties:
+        log(f"Formatting {faculty} data.")
+        source_file = os.path.join(dest_dir, f"raw_syllabus_data_{faculty}.csv")
+        dest_file = os.path.join(dest_dir, f"syllabus_data_{faculty}.csv")
+        format_syllabus_data(source_file, dest_file)
+
     log(f"Total Execution Time {time.time() - start_time:.6f} seconds")
     log("==========Scraping completed==========")
     
