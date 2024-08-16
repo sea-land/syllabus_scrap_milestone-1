080fc0885ae5165cf1ea09f8895929def6512e9d
Takaki Kaito <nbtmktrt33312@gmail.com>
2024-08-16 17:01:23 +0900

    変数名の変更

--- work/scrap.py
+++ work/scrap.py
@@ -170,13 +170,13 @@ def init_driver():
     Returns:
         WebDriverインスタンス。
     """
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-gpu")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--verbose")
-    return webdriver.Remote(command_executor="http://selenium:4444/wd/hub", options=chrome_options)
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--verbose")
+    return webdriver.Remote(command_executor="http://selenium:4444/wd/hub", options=options)
 
 # シラバスデータをスクレイピングする関数
 def scrape_syllabus_data(driver, dest_dir):
